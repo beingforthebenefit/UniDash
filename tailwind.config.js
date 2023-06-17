3d18e4929bba8f2c488d6ef16ead2cb815b18689
Gerald Todd <beingforthebenefit@gmail.com>
2023-06-17 09:50:58 -0700

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  purge: {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      './public/index.html',
+    ],
   },
-  plugins: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1a202c',
+        secondary: '#2d3748',
+        accent: '#4a5568',
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
 }
-
