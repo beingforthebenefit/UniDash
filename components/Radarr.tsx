de4c569aaa131e9e13ea58ded103e3f352e1942e
Gerald Todd <beingforthebenefit@gmail.com>
2023-06-17 09:11:30 -0700

    Add Radarr component

--- components/Radarr.tsx
+++ components/Radarr.tsx
@@ -1,56 +1,44 @@
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
-import { Button } from '@nextui-org/react';
-import React from 'react';
+import { Card } from '@nextui-org/react';
 
 interface RadarrProps {
   radarrApiKey: string;
   radarrUrl: string;
 }
 
-const Radarr: React.FC<RadarrProps> = ({ radarrApiKey, radarrUrl }) => {
-  const [movieStatus, setMovieStatus] = useState<string | null>(null);
-
-  // Example movie ID. This would typically come from the user or your app state.
-  const movieId = 1234; 
-
-  useEffect(() => {
-    fetchMovieStatus();
-  }, []);
-
-  async function fetchMovieStatus() {
-    try {
-      const response = await axios.get(`${radarrUrl}/api/movie/${movieId}`, {
-        headers: { 'X-Api-Key': radarrApiKey }
-      });
-      setMovieStatus(response.data.status);
-    } catch (error) {
-      console.error('Error fetching movie status:', error);
-    }
-  }
-
-  // Example function for controlling Radarr
-  async function refreshMovie() {
-    try {
-      await axios.post(
-        `${radarrUrl}/api/command`,
-        { name: 'RefreshMovie', movieId },
-        { headers: { 'X-Api-Key': radarrApiKey } }
-      );
-    } catch (error) {
-      console.error('Error refreshing movie:', error);
-    }
-  }
-
-  return (
-    <div className="flex flex-col gap-2">
-      <h2 className="text-2xl text-blue-600">Radarr</h2>
-      <p>Movie status: {movieStatus || 'Loading...'}</p>
-      <Button color="primary" auto onClick={refreshMovie}>
-        Refresh Movie
-      </Button>
-    </div>
-  );
+interface Movie {
+  downloaded: boolean;
+  hasFile: boolean;
 }
 
-export default Radarr;
+export default function Radarr({ radarrApiKey, radarrUrl }: RadarrProps) {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+      const res = await axios.get(`${radarrUrl}/api/v3/movie`, {
+        headers: {
+          'X-Api-Key': radarrApiKey,
+        },
+      });
+      setMovies(res.data);
+    };
+    fetchMovies();
+  }, [radarrApiKey, radarrUrl]);
+
+  const downloadedMovies = movies.filter((movie) => movie.downloaded).length;
+  const missingMovies = movies.filter((movie) => !movie.hasFile).length;
+
+  return (
+    <Card>
+      <Card.Body>
+        <h2>Radarr Stats</h2>
+        <p>Total movies: {movies.length}</p>
+        <p>Downloaded movies: {downloadedMovies}</p>
+        <p>Missing movies: {missingMovies}</p>
+      </Card.Body>
+    </Card>
+  );
+}
